0f3f765ae5933a5730eb589045860f7f1f7f2000
Josh Painter <jpainter4@gmail.com>
2015-01-12 15:06:35 -0800

    added javascript data

---
+++ js/data.js
@@ -0,0 +1,21 @@
+var cat1question1 = {
+	category: 1,
+	questionNum : 1,
+	question : 'In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?',
+	answers: ['William and Elizabeth',
+			 	'Joseph and Catherine',
+			 	'John and Mary',
+			 	'George and Anne'],
+	correctAnswerIndex : 2
+};
+
+var cat1question2 = {
+	category: 1,
+	questionNum : 2,
+	question : 'When did the Liberty Bell get its name?',
+	answers: ['When it was made, in 1701',
+				 'When it rang on July 4, 1776',
+				 'In the 19th century, when it became a symbol of the abolition of slavery',
+				 'None of the above'],
+	correctAnswerIndex : 2
+};
