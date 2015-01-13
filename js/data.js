e7d906d12e9c1a0546660087fb245e89db4e3d55
Josh Painter <jpainter4@gmail.com>
2015-01-12 20:25:30 -0800

    category3 data

--- js/data.js
+++ js/data.js
@@ -146,3 +146,302 @@ var cat1question12 = {
 				 'Groundhog Day'],
 	correctAnswerIndex : 3
 };
+
+//************************************
+// Begin Category 2, Entertainment
+//************************************
+
+var cat2question1 = {
+	category : 2,
+	questionNum : 1,
+	question : 'Which actor starred in The Color of Money?',
+	answers: ['Tom Berenger',
+				 'Tom Cruise',
+				 'Tom Hanks',
+				 'Tom Stewart'],
+	correctAnswerIndex : 1
+};
+
+var cat2question2 = {
+	category : 2,
+	questionNum : 2,
+	question : 'The actor Mel Gibson was born where?',
+	answers: ['Birmingham, England',
+				 'Peekskill, New York ',
+				 'Wagga Wagga Australia',
+				 'Sydney, Australia'],
+	correctAnswerIndex : 1
+};
+
+var cat2question3 = {
+	category : 2,
+	questionNum : 3,
+	question : 'Which actor has the real name of Charles Carter?',
+	answers: ['Chuck Norris',
+				 'Charles Atlas',
+				 'Charlton Heston',
+				 'Jimmy Carter'],
+	correctAnswerIndex : 2
+};
+
+var cat2question4 = {
+	category : 2,
+	questionNum : 4,
+	question : 'Monty Python\'s Flying Circus debuted on the BBC in what year?',
+	answers: ['1959',
+				 '1969',
+				 '1979',
+				 '1989'],
+	correctAnswerIndex : 1
+};
+
+var cat2question5 = {
+	category : 2,
+	questionNum : 5,
+	question : 'Whose autobiography was called Back In The Saddle Again?',
+	answers: ['Gene Autry',
+				 'Hopalong Cassidy',
+				 'Tom Mix',
+				 'Roy Rogers'],
+	correctAnswerIndex : 0
+};
+
+var cat2question6 = {
+	category 2: ,
+	questionNum 6: ,
+	question : 'Goldie Hawn\'s  real first name is what?',
+	answers: ['Godiva',
+				 'Goldie',
+				 'Gloria',
+				 'Grace'],
+	correctAnswerIndex : 
+};
+
+var cat2question7 = {
+	category : 2,
+	questionNum : 7,
+	question : 'What was Walt Disney\'s Mickey Mouse\'s original name?',
+	answers: ['Manville',
+				 'Melvin',
+				 'Murgatroyd',
+				 'Mortimer'],
+	correctAnswerIndex : 3
+};
+
+var cat2question8 = {
+	category : 2,
+	questionNum : 8,
+	question : 'Which of the following entertainers was the highest paid for the two years 1988 and 1989?',
+	answers: ['Bill Cosby',
+				 'Michael Jackson',
+				 'Madonna',
+				 'Oprah Winfrey'],
+	correctAnswerIndex : 1
+}; 
+
+var cat2question9 = {
+	category : 2,
+	questionNum : 9,
+	question : 'Which actress starred in the original Broadway production of Cabaret as Sally Bowles?',
+	answers: ['Julie Andrews',
+				 'Carol Channing',
+				 'Jill Haworth',
+				 'Liza Minnelli'],
+	correctAnswerIndex : 3
+};
+
+var cat2question10 = {
+	category : 2,
+	questionNum : 10,
+	question : 'Which of the following men were not married to Ava Gardner?',
+	answers: ['Richard Burton',
+				 'Mickey Rooney',
+				 'Frank Sinatra',
+				 'Artie Shaw'],
+	correctAnswerIndex : 0
+};
+
+var cat2question11 = {
+	category : 2,
+	questionNum : 11,
+	question : 'In the original Broadway production of The Unsinkable Molly Brown, who played the title role?',
+	answers: ['Barbara Cook',
+				 'Tammy Grimes ',
+				 'Angela Lansbury',
+				 'Debbie Reynolds'],
+	correctAnswerIndex : 1
+};
+
+var cat2question12 = {
+	category : 2,
+	questionNum : 12,
+	question : 'When it closed in 1990, what play on Broadway had been performed the most times?',
+	answers: ['A Chorus Line',
+				 'Annie',
+				 'Fiddler on the Roof',
+				 'Hello Dolly'],
+	correctAnswerIndex : 0
+};
+
+//************************************
+// Begin Category 3, History
+//************************************
+
+var cat3question1 = {
+	category : 3,
+	questionNum : 1,
+	question : 'Which of the following explorers reached the North Pole?',
+	answers: ['Roald Amundsen',
+				 'Robert E. Peary',
+				 'William Barents',
+				 'Adam Sound'],
+	correctAnswerIndex : 1
+};
+
+var cat3question2 = {
+	category : 3,
+	questionNum : 2,
+	question : 'History students are taught about the "the fall of Constantinople" in 1453. to who did it fall?',
+	answers: ['Christian crusaders',
+				 'Mongol hordes',
+				 'Ottoman Turks',
+				 'Romans'],
+	correctAnswerIndex : 2
+};
+
+var cat3question4 = {
+	category : 3,
+	questionNum : 4,
+	question : 'Catherine the Great ruled what country?',
+	answers: ['England',
+				 'France',
+				 'Germany',
+				 'Russia'],
+	correctAnswerIndex : 3
+};
+
+var cat3question5 = {
+	category : 3,
+	questionNum : 5,
+	question : 'What was the next state after the original 13 to be admitted to the United States?',
+	answers: ['Florida',
+				 'Kentucky',
+				 'Vermont',
+				 'West Virginia'],
+	correctAnswerIndex : 2
+};
+
+var cat3question6 = {
+	category : 3,
+	questionNum : 6,
+	question : 'The Creoles of today\'s New Orleans are descended from whom?',
+	answers: ['French Canadians from Nova Scotia',
+				 'French and Spanish settlers who lived in the city',
+				 'refugees from the Crimean War',
+				 'none of the above'],
+	correctAnswerIndex : 1
+};
+
+var cat3question7 = {
+	category : 3,
+	questionNum : 7,
+	question : 'George Washington is called the father of our country, but how many kids did Mr. Washington really have?',
+	answers: ['0',
+				 '1',
+				 '3',
+				 '7'],
+	correctAnswerIndex : 0
+};
+
+var cat3question8 = {
+	category : 3,
+	questionNum : 8,
+	question : 'What were the first names of the famous explorers Lewis and Clark?',
+	answers: ['Clark and Lewis',
+				 'John and Lincoln',
+				 'Benjamin and Samuel',
+				 'Meriwether and William'],
+	correctAnswerIndex : 3
+};
+
+var cat3question9 = {
+	category : 3,
+	questionNum : 9,
+	question : 'Where was the UN headquarters located prior to them moving to Manhattan\'s East Side?',
+	answers: ['San Francisco',
+				 'Long Island, New York',
+				 'Geneva, Switzerland',
+				 'Paris, France'],
+	correctAnswerIndex : 1
+ 
+var cat3question9 = {
+	category : 3,
+	questionNum : 9,
+	question : 'Which American commander said, "I have not yet begun to fight"?',
+	answers: ['John Paul Jones',
+				 'David Farragut',
+				 'George Dewey',
+				 'Oliver Hazard Perry'],
+	correctAnswerIndex : 0
+};
+
+var cat3question9 = {
+	category : 3,
+	questionNum : 9,
+	question : 'John Brown of Civil War fame was which?',
+	answers: ['an abolitionist',
+				 'a slave',
+				 'a slave-owner',
+				 'none of the above'],
+	correctAnswerIndex : 0 
+};
+
+var cat3question10 = {
+	category : 3,
+	questionNum : 10,
+	question : 'What frontier marshal was murdered in 1876 in Deadwood, South Dakota, by outlaw Jack McCall?',
+	answers: ['Buffalo Bill Cody',
+				 'Matt Dillon',
+				 'Wild Bill Hickok',
+				 'Bat Masterson'],
+	correctAnswerIndex : 2
+};
+
+var cat3question11 = {
+	category : 3,
+	questionNum : 11,
+	question : 'In what year did Leonard Kristensen of Norway lead the first party to land on the mainland of Antarctica?',
+	answers: ['1795',
+				 '1845',
+				 '1895',
+				 '1945'],
+	correctAnswerIndex : 2
+};
+
+var cat3question12 = {
+	category : 3,
+	questionNum : 12,
+	question : 'At the time of the Declaration of Independence what was the approximate  population of the United States?',
+	answers: ['20,000',
+				 '200,000',
+				 '2,000,000',
+				 '20,000,000'],
+	correctAnswerIndex : 2
+};
+
+/* template
+var catquestion = {
+	category : ,
+	questionNum : ,
+	question : '',
+	answers: ['',
+				 '',
+				 '',
+				 ''],
+	correctAnswerIndex : 
+};
+*/
+
+//************************************
+// Begin Category 4, Literature
+//************************************
