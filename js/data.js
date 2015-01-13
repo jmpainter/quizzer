2c022d67bb52432c096f104ca0f38a41ef5250ea
Josh Painter <jpainter4@gmail.com>
2015-01-12 17:59:27 -0800

    category 1 data

--- js/data.js
+++ js/data.js
@@ -1,3 +1,21 @@
+
+/* template
+var catquestion = {
+	category: ,
+	questionNum : ,
+	question : '',
+	answers: ['',
+				 '',
+				 '',
+				 ''],
+	correctAnswerIndex : 
+};
+*/
+
+//************************************
+// Begin Category 1, American Trivia
+//************************************
+
 var cat1question1 = {
 	category: 1,
 	questionNum : 1,
@@ -18,4 +36,113 @@ var cat1question2 = {
 				 'In the 19th century, when it became a symbol of the abolition of slavery',
 				 'None of the above'],
 	correctAnswerIndex : 2
+};
+
+var cat1question3 = {
+	category: 1,
+	questionNum : 3,
+	question : 'Which of the following items was owned by the fewest U.S. homes in 1990?',
+	answers: ['home computer',
+				 'compact disk player',
+				 'cordless phone',
+				 'dishwasher'],
+	correctAnswerIndex : 1 
+};
+
+var cat1question4 = {
+	category : 1,
+	questionNum : 4,
+	question : 'Who is third behind Hank Aaron and Babe Ruth in major league career home runs?',
+	answers: ['Reggie Jackson',
+				 'Harmon Killebrew',
+				 'Willie Mays',
+				 'Frank Robinson'],
+	correctAnswerIndex : 2 
+};
+
+var cat1question5 = {
+	category: ,
+	questionNum : ,
+	question : '',
+	answers: ['doom',
+				 'day',
+				 'Dwight (Eisenhower)',
+				 'Dunkirk'],
+	correctAnswerIndex : 1 
+};
+
+var cat1question6 = {
+	category : 1,
+	questionNum : 6,
+	question : 'The Brownie Box Camera introduced by Eastman Kodak in 1900 had a retail price of what?',
+	answers: ['$1',
+				 '$5',
+				 '$10',
+				 '$20'],
+	correctAnswerIndex : 0
+};
+
+var cat1question7 = {
+	category: 1,
+	questionNum : 7,
+	question : 'In J. Edgar Hoover, what did the J stand for?',
+	answers: ['James',
+				 'John',
+				 'Joseph',
+				 'Jerry'],
+	correctAnswerIndex : 1
+};
+
+var cat1question8 = {
+	category: 1,
+	questionNum : 8,
+	question : 'Which character,in the Wizard of Oz,does Dorothy say she\'ll miss \'most of all\' when she leaves Oz?',
+	answers: ['The Wizard',
+				 'The Scarecrow',
+				 'The Cowardly Lion',
+				 'The Tin Man'],
+	correctAnswerIndex : 1
+};
+
+var cat1question9 = {
+	category: 1,
+	questionNum : 9,
+	question : 'In the House of Representatives which state has more than one seat?',
+	answers: ['Alaska',
+				 'Hawaii',
+				 'Vermont',
+				 'Wyoming'],
+	correctAnswerIndex : 1
+};
+
+var cat1question10 = {
+	category: 1,
+	questionNum : 10,
+	question : 'The Constitution protects citizens against "unreasonable searches and seizures" with which amendment?',
+	answers: ['the fourth',
+				 'the first',
+				 'the ninth',
+				 'the fourteenth'],
+	correctAnswerIndex : 
+
+var cat1question11 = {
+	category: 1,
+	questionNum : 12,
+	question : 'What is the most popular name for a dog in the U.S. according to  Harper\'s Index Book?',
+	answers: ['Fluffy',
+				 'Rover',
+				 'Max',
+				 'Spot'],
+	correctAnswerIndex : 1
+};
+
+var cat1question12 = {
+	category: 1,
+	questionNum : 12,
+	question : 'Candlemass is known by what name in the United States?',
+	answers: ['April Fool\'s Day',
+				 'Halloween',
+				 'Thanksgiving',
+				 'Groundhog Day'],
+	correctAnswerIndex : 3
 };
